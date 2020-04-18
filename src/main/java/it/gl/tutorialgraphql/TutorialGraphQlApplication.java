9682d1545cf68d2021a3b66f692da92ebac79505
Giovanni Lofrumento <giovanni.lofrumento@gmail.com>
2020-04-18 16:27:43 +0200

    First commit

---
+++ src/main/java/it/gl/tutorialgraphql/TutorialGraphQlApplication.java
@@ -0,0 +1,13 @@
+package it.gl.tutorialgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gl/tutorialgraphql/TutorialGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gl.tutorialgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
